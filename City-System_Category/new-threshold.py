bb617642a41d6201c2ec9155f67a8a27fcead199
jonas <jonasknoll@online.de>
2026-03-03 13:06:19 +0100

    system-type

---
+++ City-System_Category/new-threshold.py
@@ -0,0 +1,20 @@
+# enter venv with: source venv/bin/activate
+import pandas as pd
+from io import StringIO
+
+data = "/Users/jonas/DHBW/BA/Bachelorarbeit_Demand-AD/City-System_Category/V1_C_network_stats.csv"
+
+# Einlesen
+df = pd.read_csv(data)
+
+# sicherstellen, dass bike_return_pct numerisch ist
+df["bike_return_pct"] = pd.to_numeric(df["bike_return_pct"], errors="coerce")
+
+# Bedingungen anwenden und system_type ersetzen
+df.loc[df["bike_return_pct"] < 8, "system_type"] = "station_based"
+df.loc[df["bike_return_pct"] > 25, "system_type"] = "free_floating"
+df.loc[(df["bike_return_pct"] >= 8) & (df["bike_return_pct"] <= 25), "system_type"] = "ungewiss"
+
+print(df)
+
+df.to_csv("/Users/jonas/DHBW/BA/Bachelorarbeit_Demand-AD/City-System_Category/V1_C_network_stats_updated.csv", index=False)
