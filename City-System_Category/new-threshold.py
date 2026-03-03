6609910626b1d563d77423b3136b0ca499176c90
jonas <jonasknoll@online.de>
2026-03-03 13:13:48 +0100

    update classification of system

--- City-System_Category/new-threshold.py
+++ City-System_Category/new-threshold.py
@@ -12,8 +12,8 @@ df["bike_return_pct"] = pd.to_numeric(df["bike_return_pct"], errors="coerce")
 
 # Bedingungen anwenden und system_type ersetzen
 df.loc[df["bike_return_pct"] < 8, "system_type"] = "station_based"
-df.loc[df["bike_return_pct"] > 25, "system_type"] = "free_floating"
-df.loc[(df["bike_return_pct"] >= 8) & (df["bike_return_pct"] <= 25), "system_type"] = "ungewiss"
+df.loc[df["bike_return_pct"] > 18, "system_type"] = "free_floating"
+df.loc[(df["bike_return_pct"] >= 8) & (df["bike_return_pct"] <= 18), "system_type"] = "ungewiss"
 
 print(df)
 
